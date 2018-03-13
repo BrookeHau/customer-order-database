5f3af1a2cc5547c65dfacfa2dbd10734be709a36
BrookeHau <brookerhauser@gmail.com>
2018-03-12 23:15:32 -0400

    created orders html page

--- src/main/java/org/wecancodeit/Customer/Order/Database/CustomerOrderController.java
+++ src/main/java/org/wecancodeit/Customer/Order/Database/CustomerOrderController.java
@@ -5,22 +5,29 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerOrderController {
 
 	@Resource
 	CustomerRepository customerRepo;
-	
+
 	@Resource
 	OrderRepository orderRepo;
-	
+
 	@Resource
 	ProductRepository productRepo;
-	
+
 	@RequestMapping("/customerorders")
 	public String findAllOrders(Model model) {
-		model.addAttribute("c", orderRepo.findAll());
+		model.addAttribute("orders", orderRepo.findAll());
 		return "customerorders";
+	}
+
+	@RequestMapping("/customer")
+	public String findCustomer(@RequestParam Long id, Model model) {
+		model.addAttribute("customers", customerRepo.findOne(id));
+		return "customer";
 	}
 }
